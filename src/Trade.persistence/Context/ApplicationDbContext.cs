8e582a1dd2ec2384f259aeb7963462346e052712
GPplay <gybramdali2002@hotmail.com>
2026-06-20 20:11:22 -0500

    Inyeccion de dependecias

---
+++ src/Trade.persistence/Context/ApplicationDbContext.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.ClientModel.Primitives;
+using System.Collections.Generic;
+using System.Reflection;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Trade.Domain.Entities;
+using Trade.persistence.Interceptors;
+
+namespace Trade.persistence.Context
+{
+    internal class ApplicationDbContext : DbContext // la clase DbCOntext detras implementa el patron Unit of Work, y el DbSet implementa el patron Repository
+    {
+        public readonly AuditableEntitySaveChangeInterceptor _auditableEntitySaveChangeInterceptor;
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options,
+                                    AuditableEntitySaveChangeInterceptor auditableEntitySaveChangeInterceptor) : base(options) {
+            {
+                _auditableEntitySaveChangeInterceptor = auditableEntitySaveChangeInterceptor;
+            }
+        }
+        
+        public DbSet<Order> Orders { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+            base.OnModelCreating(modelBuilder);
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.AddInterceptors(_auditableEntitySaveChangeInterceptor);
+            base.OnConfiguring(optionsBuilder);
+        }
+
+        public override async Task<int> SaveChangesAsync(CancellationToken cancellationToken = default)
+        {
+            return await base.SaveChangesAsync(cancellationToken);
+        }
+}
+}
+
+/*
+ * la case DbContext es la clase principal que se encarga de gestionar la conexion a la base de datos, 
+ * y de mapear las entidades a las tablas de la base de datos, ademas de implementar el patron Unit of Work, 
+ * que permite gestionar las transacciones de manera eficiente, y el DbSet implementa el patron Repository, 
+ * que permite gestionar las operaciones CRUD de manera eficiente.
+*/
---
+++ src/Trade.persistence/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Trade.persistence.Context;
+using Trade.persistence.Interceptors;
+
+namespace Trade.persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<AuditableEntitySaveChangeInterceptor>();
+            services.AddDbContext<ApplicationDbContext>(options =>
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
+                    b => b.MigrationsAssembly(typeof(ApplicationDbContext).Assembly.FullName)));
+
+            return services;
+        }
+    }
+}
